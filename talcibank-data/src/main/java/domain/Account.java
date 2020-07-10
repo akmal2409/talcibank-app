a33bcb811f9b5addb18b1bb1434088ca9a5299e9
akmal2409 <akmalalikhujaev@gmail.com>
2020-07-10 18:59:27 +0200

    Added Equals and Hash method. Closes #10. Created Entitties. Closes #7. Mapped all entitties. Closes #12

--- talcibank-data/src/main/java/domain/Account.java
+++ talcibank-data/src/main/java/domain/Account.java
@@ -2,31 +2,36 @@ package domain;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
+@EqualsAndHashCode(exclude = {"client", "transactions"})
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "accounts")
 public class Account extends BaseEntity{
 
-
+    @Column(name = "name")
     private String name;
 
-
+    @Column(name = "account_number")
     private Long number;
 
-
+    @ManyToOne
+    @JoinColumn(name = "client_id")
     private Client client;
 
-
+    @Column(name = "balance")
     private double balance;
 
-
+    @Enumerated(value = EnumType.STRING)
     private AccountType accountType;
 
-
+    @OneToMany
     private Set<Transaction> transactions = new HashSet<>();
 
 
--- talcibank-data/src/main/java/domain/Card.java
+++ talcibank-data/src/main/java/domain/Card.java
@@ -1,35 +1,42 @@
 package domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(exclude = {"client"})
 @AllArgsConstructor
 @Builder
+@Entity
+@Table
 public class Card extends BaseEntity{
 
+    @Column(name = "account_number")
     private Long number;
 
+    @ManyToOne
+    @JoinColumn(name = "client_id")
     private Client client;
 
+    @Column(name = "holders_name")
     private String holdersName;
 
+    @Column(name = "issued")
     private LocalDate issued;
 
-
+    @Column(name = "expires")
     private LocalDate expires;
 
-
+    @Enumerated(value = EnumType.STRING)
     private CardType cardType;
 
-
+    @Enumerated(value = EnumType.STRING)
     private Currency currency;
 
+    @Column(name = "balance")
     private Double balance;
 
 
