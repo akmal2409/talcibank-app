eb731cdcc8a694537075800dfc828a7c3231c01f
akmal2409 <akmalalikhujaev@gmail.com>
2020-07-10 18:06:12 +0200

    Initial commit

---
+++ src/main/java/tech/talci/talcibankapp/TalcibankAppApplication.java
@@ -0,0 +1,13 @@
+package tech.talci.talcibankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalcibankAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalcibankAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/talci/talcibankapp/TalcibankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.talci.talcibankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalcibankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
