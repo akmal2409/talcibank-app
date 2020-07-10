7e809954f56056cd2ef72128a5bea45015e85411
akmal2409 <akmalalikhujaev@gmail.com>
2020-07-10 18:23:53 +0200

    Added Project Lombok. Closes #9

---
+++ talcibank-data/src/main/java/domain/Account.java
@@ -0,0 +1,38 @@
+package domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Account extends BaseEntity{
+
+
+    private String name;
+
+
+    private Long number;
+
+
+    private Client client;
+
+
+    private double balance;
+
+
+    private AccountType accountType;
+
+
+    private Set<Transaction> transactions = new HashSet<>();
+
+
+    public void addTransaction(Transaction transaction){
+        transactions.add(transaction);
+    }
+}
