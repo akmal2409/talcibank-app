811f11f07b095980788e5d105b3ffa8596e83cfe
akmal2409 <akmalalikhujaev@gmail.com>
2020-07-18 20:14:16 +0200

    Added saveAccountCommand and wrote tests for Account controller, implemented a method to create a new bank account

---
+++ talcibank-web/src/test/java/tech/talci/talcibankapp/controller/AccountControllerTest.java
@@ -0,0 +1,18 @@
+package tech.talci.talcibankapp.controller;
+
+import static org.junit.Assert.*;
+
+public class AccountControllerTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void createNewAccount() {
+    }
+
+    @org.junit.Test
+    public void saveOrUpdate() {
+    }
+}
